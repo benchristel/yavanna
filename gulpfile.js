1f6b688633a35f4660f6928bcfe8ce7680ca0f9e
Ben Christel <ben.christel@gmail.com>
2016-06-21 18:14:55 -0700

    Make gulpfile use only pre-ES6 syntax
    - CircleCI apparently uses an older version of node

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,7 @@
 const gulp = require('gulp')
 const jasmine = require('gulp-jasmine')
 
-gulp.task('test', () => {
+gulp.task('test', function () {
   gulp.src(['index.js', 'test.js'])
     .pipe(jasmine())
 })
